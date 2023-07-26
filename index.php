924810aefa9a7a0e3dcbdce889920287d092568f
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-07-26 17:41:56 +0100

    Updating index.php

--- index.php
+++ index.php
@@ -18,13 +18,13 @@ include_once './components/ads_cards.php';
 
       <header id="header">
           <nav class="navbar navbar-expand-lg py-3">
-              <div class="container-fluid">
+              <div class="container">
                   <a class="navbar-brand" href="#"><img src="./img/coloclic.png" alt="colocation" width="200"></a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                       <li class="nav-item">
                           <a class="nav-link" href="#services">Trouver une colocation</a>
                       </li>
